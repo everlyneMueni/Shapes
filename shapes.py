667ae4d290bd1412dce3c1fc174efe1c43d298c9
student <student>
2019-04-30 19:15:56 +0300

    python test

---
+++ shapes.py
@@ -0,0 +1,68 @@
+class Circle:
+	def__init__(self,radius,area,circumference):
+		self.radius=radius
+		self.area=area
+		self.circumference=circumference
+
+
+
+	def area(self,pie,radius):
+		self.area=pie*radius*radius
+		print(area)
+
+
+	def circumference(self,pie,radius)
+		self.circumference=2pie*radius
+		print(circumference)
+
+
+
+class Squares:
+	def__init__(self,side,area,perimeter):
+		self.side=side
+		self.area=area
+		self.perimeter=perimeter
+
+	def area(self,side)
+		self.side=side*side
+		print(area)
+
+
+	def perimeter(self,side)
+		self.side=side*4
+		print(perimeter)
+
+
+class Rectangle:
+	def__init__(self,l,w,area,perimeter):
+		self.l=l
+		sel.w=w
+		self.area=area
+		self.perimeter=perimeter
+
+	def area(self,l,w)
+		self.lw=l*w
+		print(area)
+	
+
+	def perimeter(self,l,w)
+		self.l,w=2(l+w)
+		print(perimeter)	
+
+
+
+
+class Sphere:
+	def__init__(self,radius,SA,volume)
+		self.radius=radius
+		self.SA=SA
+		self.volume
+
+
+	def SA(self,radius,pie)
+		self.radius=4pie*radius*radius
+		print(SA)
+
+		def volume(self,radius,pie)
+			self.radius=4/3*pie*radius*radius*radius
+			print(volume)
